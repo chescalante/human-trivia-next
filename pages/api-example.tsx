a2fede008e296cb0454884dc768494b5c66d4fe0
Facundo De Lorenzo <fdelorenzo.dev@gmail.com>
2024-08-25 01:17:49 -0300

    removed calls from api-example

--- pages/api-example.tsx
+++ pages/api-example.tsx
@@ -9,23 +9,11 @@ export default function ApiExamplePage() {
         <em>You must be signed in to see responses.</em>
       </p>
       <h2>Game.get</h2>
-      <p>/api/examples/session</p>
+      <p>/api/game/get</p>
       <iframe src="/api/game/get" />
-      <h2>Game.finish</h2>
-      <p>/api/examples/session</p>
-      <iframe src="/api/game/finish" />
-      <h2>Game.start</h2>
-      <p>/api/examples/session</p>
-      <iframe src="/api/game/start" />
       <h2>Session</h2>
       <p>/api/trivia/get</p>
       <iframe src="/api/trivia/get" />
-      <h2>Session</h2>
-      <p>/api/trivia/join</p>
-      <iframe src="/api/trivia/join" />
-      <h2>Session</h2>
-      <p>/api/question/answer</p>
-      <iframe src="/api/question/answer" />
       <p>/api/question/get</p>
       <iframe src="/api/question/get" />
     </Layout>
